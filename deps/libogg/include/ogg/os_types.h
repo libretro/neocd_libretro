f98f0367bc2e6d6be7b56457bd40992ee0ea050f
twinaphex <libretro@gmail.com>
2021-04-20 01:01:37 +0200

    Add __LIBRETRO__ ifdef for os_types.h

--- deps/libogg/include/ogg/os_types.h
+++ deps/libogg/include/ogg/os_types.h
@@ -23,7 +23,15 @@
 #define _ogg_realloc realloc
 #define _ogg_free    free
 
-#if defined(_WIN32)
+#if defined(__LIBRETRO__)
+#    include <stdint.h>
+     typedef int16_t ogg_int16_t;
+     typedef uint16_t ogg_uint16_t;
+     typedef int32_t ogg_int32_t;
+     typedef uint32_t ogg_uint32_t;
+     typedef int64_t ogg_int64_t;
+     typedef uint64_t ogg_uint64_t;
+#elif defined(_WIN32)
 
 #  if defined(__CYGWIN__)
 #    include <stdint.h>
